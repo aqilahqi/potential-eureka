6f942d0d34ea9515a0fcb4b15d5247998a99a120
Aqilah <aqilahfatinfauzi@gmail.com>
2023-12-21 19:11:47 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import QuestionTwo from './components/QuestionTwo.vue';
+import QuestionThree from './components/QuestionThree.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Vue Assessment" />
+    </div>
+  </header>
+
+  <main>
+    <QuestionTwo />
+    <QuestionThree />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  padding-bottom: 20px;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+    padding-bottom: 0;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/QuestionTwo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <h1>Question 2</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1 {
+    color: #00bd7e;
+}
+.wrapper {
+    padding: 20px 0;
+    border-bottom: 1px solid #00bd7e;
+}
+</style>
